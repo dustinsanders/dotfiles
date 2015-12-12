3a2b5d3f9860f5482e57eb62f4c245f38b2c17cc
Dustin Sanders <dustinsanders91@gmail.com>
2015-12-11 23:59:50 -0600

    updating mac default settings

--- mac/settings.sh
+++ mac/settings.sh
@@ -1,1 +1,5 @@
-defaults write com.apple.Dock workspaces-auto-swoosh -bool NO && killall Dock
+defaults write com.apple.Dock workspaces-auto-swoosh -bool NO
+defaults write com.apple.dock mcx-expose-disabled -bool TRUE
+defaults write com.apple.loginwindow LoginwindowLaunchesRelaunchApps -bool false
+defaults write com.apple.dock mineffect -string scale
+killall Dock
