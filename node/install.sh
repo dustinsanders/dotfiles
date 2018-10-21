14c69de3710960779013c39cedc71d6ee663bee5
Dustin Sanders <dustinsanders91@gmail.com>
2018-10-21 16:00:15 -0500

    node update

--- node/install.sh
+++ node/install.sh
@@ -1,9 +1,2 @@
-source $(brew --prefix nvm)/nvm.sh
-
-nvm install stable
-nvm install 4
-
-sudo chown -R $(whoami) $(npm config get prefix)/{lib/node_modules,bin,share}
-
 echo 'Installing node modules'
-npm i -g react-native-cli
+npm i -g yarn n
