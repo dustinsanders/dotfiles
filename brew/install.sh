433207119c8211fc5280d8f3303f0182a1d2e80e
Dustin Sanders <dustinsanders91@gmail.com>
2020-10-01 08:56:24 -0500

    updating brew script

--- brew/install.sh
+++ brew/install.sh
@@ -5,11 +5,3 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 
 echo 'installing applications'
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
-
-brew cask install caffeine
-brew cask install firefox
-brew cask install brave
-brew cask install slack
-brew cask install visual-studio-code
-
-brew install node
